cb20e6bf613cf26b96faed6eb0aba0b2f59842b4
JustinL163 <Justin163@outlook.com.au>
2022-06-26 14:07:35 +1000

    fix css reference

--- planner/js/cloudAPI.js
+++ planner/js/cloudAPI.js
@@ -1,12 +1,13 @@
-let testuint;
 function saveRequest() {
     LZMA.compress(localStorage.getItem('save-data'), 9, function (result, error) {
-        testuint = new Int8Array(result);
+        let uint = new Uint8Array(result);
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://127.0.0.1:3000/save');
-        xhr.setRequestHeader('Content-Type', 'application/octet-stream');
-        xhr.send(testuint);
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('POST', 'http://127.0.0.1:3000/save');
+        // xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+        // xhr.setRequestHeader('Username', "");
+        // xhr.setRequestHeader('Auth_Key', "");
+        // xhr.send(uint);
     })
 }
 
@@ -15,4 +16,12 @@ function loadRequest() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://127.0.0.1:3000/load');
     xhr.send();
+}
+
+ToBase64 = function (u8) {
+    return btoa(String.fromCharCode.apply(null, u8));
+}
+
+FromBase64 = function (str) {
+    return atob(str).split('').map(function (c) { return c.charCodeAt(0); });
 }
