a3017aad45e12c39ccfd1920994e1c8e11c3ba86
JustinL163 <Justin163@outlook.com.au>
2022-07-03 12:37:48 +1000

    Version 1.2.4
    changed filter groups to default closed

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -404,16 +404,16 @@ function init() {
 
     colourTableRows("gear-table");
 
-    if ("1.2.3".localeCompare(data.site_version ?? "0.0.0", undefined, { numeric: true, sensitivity: 'base' }) == 1) {
+    if ("1.2.4".localeCompare(data.site_version ?? "0.0.0", undefined, { numeric: true, sensitivity: 'base' }) == 1) {
         var updateMessage = ("If anything seems broken, try 'hard refreshing' the page (google it)<br>" +
             "If still having issues, contact me on Discord, Justin163#7721");
         Swal.fire({
-            title: "Updated to Version 1.2.3",
+            title: "Updated to Version 1.2.4",
             color: alertColour,
             html: updateMessage
         })
 
-        data.site_version = "1.2.3";
+        data.site_version = "1.2.4";
         saveToLocalStorage(false);
     }
 
