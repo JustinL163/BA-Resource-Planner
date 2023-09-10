5eaf8300ca9465991dc3a4bb34d55b09ed9a2efc
JustinL163 <Justin163@outlook.com.au>
2023-09-10 23:59:00 +1000

    activate event

--- planner/js/eventPlanner.js
+++ planner/js/eventPlanner.js
@@ -65,7 +65,7 @@ let currentTab = "";
 
 function loadResources() {
 
-    $.getJSON('json/events.json?24').done(function (json) {
+    $.getJSON('json/events.json?25').done(function (json) {
         event_config = json;
         checkResources();
     });
