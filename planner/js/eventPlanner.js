fd13b45aadc9bf4de38fe06a2b000475ea66ef21
JustinL163 <Justin163@outlook.com.au>
2024-03-12 17:46:44 +1100

    minor fix

--- planner/js/eventPlanner.js
+++ planner/js/eventPlanner.js
@@ -71,7 +71,7 @@ let currentTab = "";
 
 function loadResources() {
 
-    $.getJSON('json/events.json?41').done(function (json) {
+    $.getJSON('json/events.json?42').done(function (json) {
         event_config = json;
         checkResources();
     });
