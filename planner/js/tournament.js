800994fc0b482f7264af574539a28aeac31f7ce0
JustinL163 <Justin163@outlook.com.au>
2024-12-10 23:06:52 +1100

    update tournament page

--- planner/js/tournament.js
+++ planner/js/tournament.js
@@ -18,7 +18,7 @@ let lastLoadDisplay;
 
 let maxBans = 5;
 
-let dTournaments = { "tournament-1": "S2 ShiroKuro", "tournament-2": "S3 Kaiten", "tournament-3": "S4 Hod", "tournament-5": "S5 Goz", "tournament-6": "S6 Goz" };
+let dTournaments = { "tournament-1": "S2 ShiroKuro", "tournament-2": "S3 Kaiten", "tournament-3": "S4 Hod", "tournament-5": "S5 Goz", "tournament-6": "S6 Goz", "tournament-7": "S7 Wakamo" };
 let dStages = { "qualifiers": "Qualifiers", "groups": "Groups", "elimination": "Elimination" };
 let dGames = { "game-1": "1", "game-2": "2", "game-3": "3", "game-4": "4" };
 let dArmour = { "light": "Light", "heavy": "Heavy", "special": "Special", "elastic": "Elastic" };
