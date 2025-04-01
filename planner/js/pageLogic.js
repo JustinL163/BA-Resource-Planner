64bd3f06864de8cc33ab8314ffd4c0167f4fc0a2
JustinL163 <Justin163@outlook.com.au>
2025-04-01 23:17:57 +1100

    minor fix

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -422,14 +422,14 @@ function init() {
 
     colourTableRows("gear-table");
 
-    if ("1.4.12".localeCompare(data.site_version ?? "0.0.0", undefined, { numeric: true, sensitivity: 'base' }) == 1) {
+    if ("1.4.13".localeCompare(data.site_version ?? "0.0.0", undefined, { numeric: true, sensitivity: 'base' }) == 1) {
         Swal.fire({
-            title: GetLanguageString("text-updatedversionprefix") + "1.4.12",
+            title: GetLanguageString("text-updatedversionprefix") + "1.4.13",
             color: alertColour,
             html: GetLanguageString("text-updatemessage")
         })
 
-        data.site_version = "1.4.12";
+        data.site_version = "1.4.13";
         // saveToLocalStorage(false);
     }
 
